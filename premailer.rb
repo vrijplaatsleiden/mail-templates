33f10da2f16df2bd1c6922fe6c4711f00e5fa16e
David Heijkamp <david.heijkamp@naturalis.nl>
2014-08-30 13:50:42 +0200

    Added option to include more snippets

--- premailer.rb
+++ premailer.rb
@@ -9,19 +9,46 @@ require 'trollop'
 
 # Load configuration file
 # TODO: Add an option to overrule the default ini-file.
+# TODO: Check if ini-file exists
 config = IniFile.load('premailer.ini')
 
 
 # Overrule the default message options in the ini-file with command arguments
-# TODO: Add a content snippet option, possibly including multiple snippets
 opts = Trollop::options do
-  opt :to, "Address to send the mail to", :default => config['message']['to']
-  opt :from, "Address to send the mail from", :default => config['message']['from']
-  opt :subject, "Subject of the mail", :default => config['message']['subject']
-  opt :template, "Template file to use", :default => config['message']['template']
-  opt :content, "Content file to use", :default => config['message']['content']
+  opt :to, "Address to send the mail to", :type => :string, :default => config['message']['to']
+  opt :from, "Address to send the mail from", :type => :string, :default => config['message']['from']
+  opt :subject, "Subject of the mail", :type => :string, :default => config['message']['subject']
+  opt :title, "Title to use in the mail (default: same as subject)", :type => :string
+  opt :template, "Master template file to use", :type => :string, :default => config['message']['template']
+  opt :content, "Content file to use", :type => :string, :default => config['message']['content']
+  opt :header, "Optional header file to use", :type => :string, :default => config['message']['header']
+  opt :footer, "Optional footer file to use", :type => :string, :default => config['message']['footer']
 end
 
+# Check if specified files actually exist
+unless File.file?(opts[:template])
+  puts "The template file you specified doesn't exist."
+  exit
+end
+
+unless File.file?(opts[:content])
+  puts "The content file you specified doesn't exist."
+  exit
+end
+
+if opts[:header]
+  unless File.file?(opts[:header])
+    puts "The header file you specified doesn't exist."
+    exit
+  end
+end
+
+if opts[:footer]
+  unless File.file?(opts[:footer])
+    puts "The footer file you specified doesn't exist."
+    exit
+  end
+end
 
 # Set the mail server options
 options = { :address              => config['server']['address'],
@@ -31,14 +58,33 @@ options = { :address              => config['server']['address'],
             :enable_starttls_auto => config['server']['enable_starttls_auto']
 }
 
+## Apply the snippets and content to the template
+mail = File.read(opts[:template])
 
-# Apply the content to the template
-# TODO: Extend this option to include more snippets like introduction, footer etc.
-mail = File.read('template.html').gsub("%CONTENT%", File.read(opts[:content]))
+# Add title based on argument, ini-file or, when both are lacking, the subject
+if opts[:title]
+  mail = mail.gsub("<!-- TITLE -->", opts[:title])
+else
+  mail = mail.gsub("<!-- TITLE -->", opts[:subject])
+end
+
+# Add header
+if opts[:header]
+  mail = mail.gsub("<!-- HEADER -->", File.read(opts[:header]))
+end
+
+# Add footer
+if opts[:footer]
+  mail = mail.gsub("<!-- FOOTER -->", File.read(opts[:footer]))
+end
+
+# And always add content
+mail = mail.gsub("<!-- CONTENT -->", File.read(opts[:content]))
+
 premailer = Premailer.new(mail, :warn_level => Premailer::Warnings::SAFE, :with_html_string => true)
 
 
-# Send the mail with configured options
+## Send the mail with configured options
 Mail.defaults do
   delivery_method :smtp, options
 end
